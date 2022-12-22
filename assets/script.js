f79c73c63246dc8a39f7be04fea49c22b19bda77
issam <issammez44@gmail.com>
2022-12-22 14:12:26 +0100

    change between info section and quiz section

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,12 @@ const one = document.querySelector(".one") ;
 const two = document.querySelector(".two") ;
 const three = document.querySelector(".three") ;
 const quizPage = document.getElementById("quizP") ;
+const infoSection = document.getElementById("info_section") ;
+const quizSection = document.getElementById("quiz_section") ;
+const resultSection = document.getElementById("result_section") ;
 
 quizPage.onclick = function() {
     two.classList.add("active") ;
+    infoSection.style.display="none" ; 
+    quizSection.style.display="flex" ; 
 }
