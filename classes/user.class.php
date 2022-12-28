794140d898b9b78e8d2fcab243c4758badc91e74
issam <issammez44@gmail.com>
2022-12-28 17:34:03 +0100

    add structer of user and admin classes

---
+++ classes/user.class.php
@@ -0,0 +1,16 @@
+<?php
+    class user extends person {
+        public function signUp(){
+
+        }
+        public function login(){
+
+        }
+        public function showQuestions(){
+
+        }
+        public function scoreHistory(){
+            
+        }
+    }
+?>
